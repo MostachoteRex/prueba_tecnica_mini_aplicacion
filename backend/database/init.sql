340bcf46ffc267122b395de05ce25c0040bc30c3
MostachoteRex <108437393+MostachoteRex@users.noreply.github.com>
2025-12-12 01:46:45 -0500

    Delete backend/database/init.sql

--- backend/database/init.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO items_compra (nombre, cantidad, comprado) VALUES
-('Manzanas', 5, false),
-('Leche', 2, true),
-('Pan', 1, false),
-('Huevos', 12, false),
-('Café', 1, false),
-('Arroz', 2, true)
-ON CONFLICT DO NOTHING;
